749bd90f4ad615e862c7fe15b70954a16038b51a
Shivam Shishodia <theshishodia@gmail.com>
2022-02-21 18:50:23 +0530

    Initial commit

---
+++ src/test/java/com/shishodia/quartz/emailscheduler/EmailSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shishodia.quartz.emailscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
